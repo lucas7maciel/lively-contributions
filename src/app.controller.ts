5e0d0b9af953262c53820cd58ad57218c75e3e98
Lucas Maciel <lucasmacielcontato@gmail.com>
2023-12-27 14:00:36 -0300

    To usando o ejs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,24 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("teste")
+  itsWorking(): string {
+    return this.appService.itsWorking();
+  }
+
+  @Get("visao")
+  @Render('teste')
+  async teste(): Promise<object> {
+    const weeks = await this.appService.getContsByDay();
+
+    console.log(weeks[0])
+    return {weeks: weeks};
+  }
+
+  @Get("contributions")
+  async contributions(): Promise<any> {
+    return this.appService.getContsByDay();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,63 @@ export class AppService {
   getHello(): string {
     return 'Hello Niggas!';
   }
+
+  itsWorking(): string {
+    return "Its workingggg";
+  }
+
+  async getContributions(): Promise<any> {
+    const body:Object = {
+      "query": `query {
+          user(login: "Eliezir") {
+            name
+            contributionsCollection {
+              contributionCalendar {
+                colors
+                totalContributions
+                weeks {
+                  contributionDays {
+                    color
+                    contributionCount
+                    date
+                    weekday
+                  }
+                  firstDay
+                }
+              }
+            }
+          }
+        }`
+  }
+
+  const params:Object = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "Authorization": `Bearer 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`
+    },
+    body: JSON.stringify(body),
+  };
+  
+    const res = await fetch("https://api.github.com/graphql", params)
+    
+    return res.json();
+  }
+
+  async getContsByDay(): Promise<any> {
+    const contributions: Object = await this.getContributions()
+    const weeks = contributions["data"]["user"]["contributionsCollection"]["contributionCalendar"]["weeks"]
+
+    //transformar em typescript
+    const formatedWeeks = weeks.map((week, index) => (
+      week["contributionDays"].map((day, index) => {
+        return {
+          "conts" : day.contributionCount,
+          "date" : day.date
+        }})
+    ))
+
+    return formatedWeeks;
+  }
 }
